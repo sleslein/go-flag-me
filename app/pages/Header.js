91c023423738e571e908594c7d5ce8eccfc91f20
sleslein <shea.leslein@gmail.com>
2020-12-24 13:39:26 -0600

    removing Next.js demo, adding subscriptions page

---
+++ app/pages/Header.js
@@ -0,0 +1,7 @@
+export function Header() {
+    return (
+        <>
+        <h1>Go Flag Me!</h1>
+        </>
+    );
+}
---
+++ app/pages/subscriptions/index.js
@@ -0,0 +1,3 @@
+export default function Subscriptions () {
+    return <h1>Welcome to the Subscrptions Page</h1>
+}
